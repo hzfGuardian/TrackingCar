05c8d01b1f355740919ac55c7267ffeb4aaec59c
Zhuofei Huang <412571747@qq.com>
2016-05-09 14:40:45 +0800

    Initial Commit

---
+++ KernelBased/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  KernelBased
+//
+//  Created by hzfmacbook on 5/9/16.
+//  Copyright © 2016 hzfmacbook. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
