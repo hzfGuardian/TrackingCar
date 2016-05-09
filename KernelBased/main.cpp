452e2658e57d70e20f33f8cc1500affe850736fd
hzfmacbook <huangzf@MacBook-Pro.local>
2016-05-09 15:21:58 +0800

    Initial Commit

--- KernelBased/main.cpp
+++ KernelBased/main.cpp
@@ -14,7 +14,27 @@ using namespace cv;
 
 int main(int argc, const char * argv[])
 {
+    Mat orig, current;
     
+    VideoCapture video_capture("move.mpeg");
+    
+    //首先采用视频序列第 1 帧图像 F0 作为初始背景
+    video_capture >> orig;
+    
+    imshow("image", orig);
+    
+    while (true) {
+        
+        if (!video_capture.read(current))
+            break;
+        
+        
+    }
+    
+
+    cout << video_capture.get(CV_CAP_PROP_FRAME_COUNT) << endl;
+    
+    waitKey();
     
     return 0;
 }
